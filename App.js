aff6d1f29d1db0af6b35a3587d601d0964b7cc89
Juan Huttemann <juanfhuttemann@gmail.com>
2021-08-18 11:43:15 -0400

    finish car item component

--- App.js
+++ App.js
@@ -5,7 +5,12 @@ import CarItem from "./components/CarItem";
 export default function App() {
   return (
     <View style={styles.container}>
-      <CarItem />
+      <CarItem
+        name="Model X"
+        tagline="Order Online For"
+        taglineCTA="Touchless Delivery"
+        image={require("./assets/images/ModelX.jpeg")}
+      />
     </View>
   );
 }
--- components/CarItem/index.js
+++ components/CarItem/index.js
@@ -3,31 +3,32 @@ import { Text, View, ImageBackground } from "react-native";
 import StyledButton from "../StyledButton";
 import styles from "./styles";
 
-export default () => {
+export default ({ name, tagline, taglineCTA, image }) => {
   return (
     <View style={styles.carContainer}>
-      <ImageBackground
-        source={require("../../assets/images/ModelS.jpeg")}
-        style={styles.image}
-      />
+      <ImageBackground source={image} style={styles.image} />
       <View style={styles.titles}>
-        <Text style={styles.title}>Model S</Text>
-        <Text style={styles.subtitle}>Starting at $69,420</Text>
+        <Text style={styles.title}>{name}</Text>
+        <Text style={styles.subtitle}>
+          {tagline} <Text style={styles.subtitleCTA}>{taglineCTA}</Text>
+        </Text>
       </View>
-      <StyledButton
-        type="primary"
-        content="Custom Order"
-        onPress={() => {
-          console.warn("Primary button");
-        }}
-      />
-      <StyledButton
-        type="secondary"
-        content="Existing Inventory"
-        onPress={() => {
-          console.warn("Secondary button");
-        }}
-      />
+      <View style={styles.btnContainer}>
+        <StyledButton
+          type="primary"
+          content="Custom Order"
+          onPress={() => {
+            console.warn("Primary button");
+          }}
+        />
+        <StyledButton
+          type="secondary"
+          content="Existing Inventory"
+          onPress={() => {
+            console.warn("Secondary button");
+          }}
+        />
+      </View>
     </View>
   );
 };
