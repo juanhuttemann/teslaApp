54bb64c0622529ad6403c30519677e55af3d8c61
Juan Huttemann <juanfhuttemann@gmail.com>
2021-08-18 09:06:39 -0400

    add car item component

---
+++ components/CarItem/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { Text, View, ImageBackground } from "react-native";
+import styles from "./styles";
+
+export default () => {
+  return (
+    <View style={styles.carContainer}>
+      <ImageBackground
+        source={require("../../assets/images/ModelS.jpeg")}
+        style={styles.image}
+      />
+      <View style={styles.titles}>
+        <Text style={styles.title}>Model S</Text>
+        <Text style={styles.subtitle}>Starting at $69,420</Text>
+      </View>
+    </View>
+  );
+};
